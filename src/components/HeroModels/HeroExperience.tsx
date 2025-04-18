d81f917d10bf422e4d59a2a1bb3987c289b149d9
Victor Aremu <victoraremu123@gmail.com>
2025-04-18 23:20:35 +0000

    Update package dependencies and enhance HeroExperience component with responsive design features

--- src/components/HeroModels/HeroExperience.tsx
+++ src/components/HeroModels/HeroExperience.tsx
@@ -1,12 +1,14 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useMediaQuery } from "react-responsive";
+import { Room } from "./Optimized-room";
+import HeroLights from "./HeroLights";
 
 const HeroExperience = () => {
-  const isTablet = useMediaQuery();
+  const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
   return (
     <Canvas camera={{ fov: 45, position: [0, 0, 15] }}>
-      <ambientLight intensity={0.2} color="#1a1a40" />
-      <directionalLight position={[5, 5, 5]} intensity={5} />
       <OrbitControls
         enablePan={false}
         enableZoom={!isTablet}
@@ -15,10 +17,14 @@ const HeroExperience = () => {
         minPolarAngle={Math.PI / 5}
         maxPolarAngle={Math.PI / 2}
       />
-      <mesh>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="teal" />
-      </mesh>
+      <HeroLights />
+      <group
+        scale={isMobile ? 0.7 : 1}
+        position={[0, -3.5, 0]}
+        rotation={[0, Math.PI / -5, 0]}
+      >
+        <Room />
+      </group>
     </Canvas>
   );
 };
---
+++ src/components/HeroModels/Optimized-room.d.ts
@@ -0,0 +1,5 @@
+declare module "./Optimized-room" {
+
+  export function Model(props: JSX.IntrinsicElements["group"]): JSX.Element;
+  export const Room: typeof Model;
+}
